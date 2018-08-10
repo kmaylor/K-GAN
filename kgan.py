4f88bab733924df36d957f0fea7c9e88c797a7d6
kmaylor <kmaylor@ucdavis.edu>
2018-08-10 16:47:56 -0700

    added comments to kgan

--- kgan.py
+++ kgan.py
@@ -20,6 +20,7 @@ from keras.initializers import TruncatedNormal
 from keras.utils import multi_gpu_model
 import tensorflow as tf
 from keras import backend as K
+
 class KGAN(object):
     def __init__(self, img_rows, img_cols, channel=1,
                     load_dir =None,
@@ -54,20 +55,29 @@ class KGAN(object):
                 print('State '+str(load_dir)+' not found, begin with fresh state?')
 
     def discriminator(self):
+    	'''Create the discriminator if it does not already exist'''
         if self.D:
             return self.D
+        
+        #initialize weights from normal distribution with 1-sigma cutoff
         initial = TruncatedNormal(0,0.02)
-        self.D = Sequential(name='Discriminator')
+
+        # depth*scale_depth give the number of features for each layer
         depth = self.depth
         if isinstance(self.depth_scale,types.FunctionType):
             depth_scale = self.depth_scale()
         else:
             depth_scale = self.depth_scale
-        input_shape = (self.img_rows, self.img_cols, self.channel)
         
+        input_shape = (self.img_rows, self.img_cols, self.channel)
+
+        # Discriminator is sequential model
+        self.D = Sequential(name='Discriminator')
+        # First layer of discriminator i a convolution, minimum of two convolutional layers
         self.D.add(Conv2D(depth*depth_scale[0], self.kernels[0], strides=self.strides[0], \
                     input_shape=input_shape, padding='same', kernel_initializer=initial, name = 'Conv2D_1'))
         
+        # Iterate over layers defined by the number of kernels and strides
         for i,ks in enumerate(zip(self.kernels[1:],self.strides[1:])):
             self.D.add(LeakyReLU(alpha=0.2, name = 'LRelu_D%i'%(i+1)))
             self.D.add(BatchNormalization(momentum=0.9, name = 'BN_D%i'%(i+1)))
@@ -77,8 +87,8 @@ class KGAN(object):
         self.D.add(LeakyReLU(alpha=0.2, name = 'LRelu_D%i'%(i+2)))
         self.D.add(BatchNormalization(momentum=0.9, name = 'BN_D%i'%(i+2)))
         
-        #self.D.add(Dropout(dropout))
-        # Out: 1-dim probability
+        # Flatten final features and calculate the probability of the input belonging to the same 
+        # as the training set
         self.D.add(Flatten(name = 'Flatten'))
         self.D.add(Dense(1, kernel_initializer=initial, name = 'Dense_D'))
         self.D.add(Activation('sigmoid', name = 'Sigmoid'))
@@ -86,9 +96,11 @@ class KGAN(object):
         return self.D
 
     def generator(self):
+    	'''Create the generator if it does not already exist'''
         if self.G:
             return self.G
         
+        # Define the bi-linear upsampling layer using resize_images imported from tensorflow
         def UpSampling2DBilinear(stride, **kwargs):
             def layer(x):
                 input_shape = K.int_shape(x)
@@ -96,27 +108,34 @@ class KGAN(object):
                 return K.tf.image.resize_images(x, output_shape, align_corners=True,
                         method = K.tf.image.ResizeMethod.BILINEAR)
             return Lambda(layer, **kwargs)
-
+        
+        #initialize weights from normal distribution with 1-sigma cutoff
         initial = TruncatedNormal(0,0.02)
-        self.G = Sequential(name='Generator')
+        
+        # depth/2*scale_depth give the number of features for each layer
         depth = int(self.depth/2)
         if isinstance(self.depth_scale,types.FunctionType):
             depth_scale = self.depth_scale()[::-1]
         else:
             depth_scale = self.depth_scale[::-1]
-
+        
+        #Get the size of the initial features from the size of the final feature layer in 
+        #the discriminator
         dim1 = self.discriminator().get_layer('Flatten').input_shape[1]
         dim2 = self.discriminator().get_layer('Flatten').input_shape[2]
         
-
+        # Generator is sequential model
+        self.G = Sequential(name='Generator')
+        # First layer of generator is densely connected
         self.G.add(Dense(dim1*dim2*depth*depth_scale[0], input_dim=self.input_dim,
                         kernel_initializer=initial, name = 'Dense_G'))
         self.G.add(LeakyReLU(alpha=0.2, name = 'LRelu_G_1'))
         self.G.add(Reshape((dim1, dim2, depth*depth_scale[0]),name='Reshape'))
+
+        # Iterate over layers defined by the number of kernels and strides
         for i,ks in enumerate(zip(self.kernels,self.strides)):
             self.G.add(BatchNormalization(momentum=0.9, name = 'BN_G%i'%(i+1)))
             if i < len(self.kernels)-1:
-                #self.G.add(UpSampling2D(ks[1]))
                 self.G.add(UpSampling2DBilinear(ks[1],name='BiL_%i'%(i+1)))
                 self.G.add(Conv2DTranspose(depth*depth_scale[i+1], ks[0], strides = 1, padding='same',
         	            kernel_initializer=initial, name = 'ConvTr2D_%i'%(i+1)))
@@ -127,6 +146,8 @@ class KGAN(object):
         	            kernel_initializer=initial, name = 'ConvTr2D_%i'%(i+1)))
                 self.G.add(Activation('tanh', name = 'Tanh'))
         
+        # If the output of the last layer is larger than the input for the discriminator crop
+        # the image
         crop_r = int((self.G.get_layer('Tanh').output_shape[1]-self.img_rows)/2)
         crop_c = int((self.G.get_layer('Tanh').output_shape[2]-self.img_cols)/2)
         self.G.add(Cropping2D(cropping=((crop_c,crop_c),(crop_r,crop_r)), name = 'Crop2D'))
@@ -135,9 +156,15 @@ class KGAN(object):
 
     
     def discriminator_model(self):
+    	'''Build and compile the discriminator model from the discriminator. This allows for 
+    	easy saving and reloading of models'''
         if self.DM:
             return self.DM
+        
+        # Adam optimizer
         optimizer = Adam(lr=0.0002,beta_1=0.5, decay=0)
+
+        # Compile the discriminator model on the number of specified gpus
         if self.gpus <=1:
             self.DM = Sequential(name = 'Discriminator Model')
             self.DM.add(self.discriminator())
@@ -155,13 +182,20 @@ class KGAN(object):
         return self.DM
 
     def adversarial_model(self):
+    	'''Build and compile the adversarial model from the discriminator and generator. Stacks the 
+    	discriminator on the generator'''
         if self.AM:
             return self.AM
+
+        # Adam optimizer    
         optimizer = Adam(lr=0.0002,beta_1=0.5, decay=0)
+
+        # Compile the gemerator model on the number of specified gpus
         if self.gpus <=1:
             self.AM = Sequential(name = 'Adversarial Model')
             self.AM.add(self.generator())
             discriminator =self.discriminator()
+            # Only use the discriminator to evaluate the generator's output
             discriminator.trainable=False
             self.AM.add(discriminator)
             self.AM.compile(loss='binary_crossentropy', optimizer=optimizer,\
@@ -171,6 +205,7 @@ class KGAN(object):
                 self.AM = Sequential(name = 'Adversarial_Model')
                 self.AM.add(self.generator())
                 discriminator =self.discriminator()
+                # Only use the discriminator to evaluate the generator's output
                 discriminator.trainable=False
                 self.AM.add(discriminator)
             self.AM = multi_gpu_model(self.AM,gpus=self.gpus)
@@ -178,10 +213,14 @@ class KGAN(object):
                 metrics=['binary_accuracy'])
 
         self.AM.summary()
+        # Set the discriminator back to trainable so the discriminator is in the correct state when reloading
+        # a model
         discriminator.trainable=True
         return self.AM
 
     def save_state(self):
+    	'''Save only the discriminator and generator DM and AM are re-compiled, allows you to change learning rates
+    	during training and add extra layers (this feature still needs to be added'''
         if not exists(str(self.save_dir)): makedirs(str(self.save_dir))
         model_type = ['D', 'G']
         for m in model_type:
@@ -193,6 +232,8 @@ class KGAN(object):
     		#model.save_weights(m+"_weights.h5")
                         
     def load_state(self):
+    	'''Load only the discriminator and generator, DM and AM are re-compiled, allows you to change learning rates
+    	during training and add extra layers (this feature still needs to be added'''
     	model_type = ['D', 'G']
     	for m in model_type:
     		setattr(self,m,load_model(str(self.load_dir)+'/'+m+'_model.h5'))
@@ -226,7 +267,16 @@ class KGAN(object):
                     train_steps=2000, batch_size=32,
                     save_interval=100, verbose = 10,
                     samples=16):
-
+        '''Trains the generator and discriminator on x_train for batches = train_steps
+        filename: path to where to save the sample images
+        train_rate: iterable containing number of times to train the discriminator and the generator in that order
+        train_steps: number of batches to train on before stopping
+        batch_size: number of samples to train discriminator and generator on each step
+        save_interval: number of steps afterwhich the models and samples will be saved
+        verbose: number of steps afterwhich the discriminator and adversarial model loss and accuracy will be d
+                 printed
+        samples: number of images in output plot
+        '''
         self.G=self.generator()
         self.DM=self.discriminator_model()
         self.AM=self.adversarial_model()
@@ -266,14 +316,23 @@ class KGAN(object):
                 if (i+1)%save_interval==0:
                     self.save_state()
                     fn = filename+"_%d.png" % (i+1)
-                    self.plot_images(images_real, images_fake, filename=fn, samples=samples)
+                    self.plot_images(fake=images_fake, real=images_real, filename=fn, samples=samples)
 
-    def plot_images(self, real, fake,seed=True, filename=None, samples=16):
-        if seed:
-            np.random.seed(1)
+    def plot_images(self, fake=None, real=None, seed=None, filename=None, samples=16):
+    	'''plot samples from the generator or the training data
+    	fake: List of images from the generator overridden if seed != None
+    	real: List of images from the training set
+    	seed: Fix value to always produce the same images from the generator
+    	filename: Path to where to save images
+    	samples: Number of images to plot'''
+        if seed!=None:
+            np.random.seed(seed)
             noise = np.random.normal(loc=0., scale=1., size=[16, self.input_dim])
             fake = self.G.predict(noise)
-        images = np.concatenate((fake[:int(samples/2)],real[:int(samples/2)]))
+        if real!=None:
+            images = np.concatenate((fake[:int(samples/2)],real[:int(samples/2)]))
+        else:
+        	images = fake
         plt.figure(figsize=(10,10))
         for i in range(images.shape[0]):
             plt.subplot(int(samples**.5), int(samples**.5), i+1)
